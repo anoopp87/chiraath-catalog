334e078f54d323b672a801afe31077cfa61e7f02
Anoop Premachandran <anooppremachandran@MacBookAir.lan>
2026-01-21 21:55:25 -0500

    Deploy Netlify headers for admin

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_headers");
 
   return {
     dir: {
