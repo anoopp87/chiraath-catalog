d83d67b0240efbb66df82e7a0e0476406764b50c
Anoop Premachandran <anooppremachandran@MacBookAir.lan>
2026-01-21 19:25:21 -0500

    Eleventy setup working

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      data: "_data"
+    }
+  };
+};
