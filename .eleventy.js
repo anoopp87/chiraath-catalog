f34cf2395bea760d1f1540862eef8f43da11b5dd
Anoop Premachandran <anooppremachandran@MacBookAir.lan>
2026-01-25 19:36:24 -0500

    Enable products collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("_headers");
+
+  // 🔴 THIS IS THE IMPORTANT LINE
+  eleventyConfig.addCollection("products", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/products/*.md");
+  });
 
   return {
     dir: {
